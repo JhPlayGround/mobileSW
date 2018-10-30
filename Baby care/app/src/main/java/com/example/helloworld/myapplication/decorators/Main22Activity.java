db867c00765ee1e27f6762351a1001263cc377d4
Jijuhwan <jijuhuan@gmail.com>
2018-10-30 11:34:25 +0900

    다시 받아보세요! activity_main22,Main22Activity 삭제

--- Baby care/app/src/main/java/com/example/helloworld/myapplication/decorators/Main22Activity.java
+++ Baby care/app/src/main/java/com/example/helloworld/myapplication/decorators/Main22Activity.java
@@ -3,11 +3,13 @@ package com.example.helloworld.myapplication.decorators;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+import com.example.helloworld.myapplication.activity.R;
+
+public class Main22Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main22);
     }
 }
