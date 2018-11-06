9d3de077e6c73542914cce1d81e8cf6ba2ebcee6
juho1210 <wngh9512@gmail.com>
2018-11-06 10:13:19 +0900

    경고 이미지 추가, 이미지 클릭어블 설정, 이미지 클릭시 토스트 되게 해놓음

--- Baby care/app/src/main/java/com/example/helloworld/myapplication/activity/DailyActivity.java
+++ Baby care/app/src/main/java/com/example/helloworld/myapplication/activity/DailyActivity.java
@@ -1,6 +1,8 @@
 package com.example.helloworld.myapplication.activity;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,9 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.helloworld.myapplication.R;
+
+import java.util.Calendar;
 
 public class DailyActivity extends Fragment {
     MainActivity activity;
@@ -36,15 +42,27 @@ public class DailyActivity extends Fragment {
 
         ViewGroup view = (ViewGroup)inflater.inflate(R.layout.main_daily,mainFragmentLayout,false);
 
-        final CalendarView calendar = (CalendarView)view.findViewById(R.id.calendar);
+        CalendarView calendar = (CalendarView)view.findViewById(R.id.calendar);
+
+        ImageView warningImg = (ImageView)view.findViewById(R.id.warning);
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getContext(), ""+year+"."+(month+1)+"." +dayOfMonth, Toast.LENGTH_SHORT).show();
+                String StrDay = year+"."+(month+1)+"."+dayOfMonth;
+                String food = "오늘의 영양소 탄수화물 \n 추천 재료 : 쌀 / 대채 재료 : 밀가루";
+                Toast.makeText(getContext(), food, Toast.LENGTH_SHORT).show();
+            }
+     });
+        warningImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "123", Toast.LENGTH_SHORT).show();
+
             }
         });
 
         return view;
     }
+
 }
