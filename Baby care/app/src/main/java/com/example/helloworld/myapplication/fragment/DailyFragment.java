579ae59d1deb35fa78ac0b4aa754c59d77ea37a7
juho1210 <wngh9512@gmail.com>
2018-11-17 15:47:42 +0900

    데일리 이유식 수정

--- Baby care/app/src/main/java/com/example/helloworld/myapplication/fragment/DailyFragment.java
+++ Baby care/app/src/main/java/com/example/helloworld/myapplication/fragment/DailyFragment.java
@@ -1,26 +1,32 @@
 package com.example.helloworld.myapplication.fragment;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 import com.example.helloworld.myapplication.R;
 import com.example.helloworld.myapplication.activity.MainActivity;
+import com.example.helloworld.myapplication.util.PopUpActivity;
+import com.github.chrisbanes.photoview.PhotoView;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 
 public class DailyFragment extends Fragment {
     MainActivity activity;
-    private PopupWindow mPopupWindow;
 
     @Override
     public void onAttach(Context context){
@@ -41,9 +47,7 @@ public class DailyFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup mainFragmentLayout, @Nullable Bundle savedInstanceState) {
 
         ViewGroup view = (ViewGroup)inflater.inflate(R.layout.main_daily,mainFragmentLayout,false);
-
         CalendarView calendar = (CalendarView)view.findViewById(R.id.calendar);
-
         ImageView helpImg = (ImageView)view.findViewById(R.id.help);
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -57,14 +61,19 @@ public class DailyFragment extends Fragment {
         helpImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View popupView = getLayoutInflater().inflate(R.layout.activity_popup, null);
-                mPopupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                mPopupWindow.setFocusable(true);
-                mPopupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+                Context mContext = getContext();
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                View layout = inflater.inflate(R.layout.activity_popup, (ViewGroup) v.findViewById(R.id.popup));
+                AlertDialog.Builder aDialog = new AlertDialog.Builder(getContext());
+
+                aDialog.setTitle("개월 수 별 먹어도 되는 음식들");
+                aDialog.setView(layout);
+
+                AlertDialog ad = aDialog.create();
+                ad.show();
             }
         });
 
         return view;
     }
-
 }
