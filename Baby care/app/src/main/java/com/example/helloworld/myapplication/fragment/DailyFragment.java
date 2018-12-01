08276e43c9227e887cdbc0b7151cd54c727e41a8
juho1210 <wngh9512@gmail.com>
2018-12-01 19:01:21 +0900

    아이 안태어남 공지, 달력 숫자 이상하게 표기 되는것 수정

--- Baby care/app/src/main/java/com/example/helloworld/myapplication/fragment/DailyFragment.java
+++ Baby care/app/src/main/java/com/example/helloworld/myapplication/fragment/DailyFragment.java
@@ -69,11 +69,12 @@ public class DailyFragment extends Fragment {
 
                 inyear =inyear - 2000;
                 int year = inyear - iyear;
-                int month = inmonth - imonth;
+                int month = (inmonth+1) - imonth;
 
                 babymonth = (year*12) + month;
+                babymonth = babymonth;
 
-                if(babymonth < 4)
+                if(babymonth >= 0 && babymonth < 4 )
                 {
                     Toast.makeText(getContext(), "오늘의 이유식 : 모유, 분유", Toast.LENGTH_SHORT).show();
                 }
@@ -1034,7 +1035,7 @@ public class DailyFragment extends Fragment {
 
                     }
                 }
-                else
+                else if(babymonth >= 24)
                 {
                     if(dayOfMonth % 9 == 1) //1,10,19,28
                     {
@@ -1105,6 +1106,10 @@ public class DailyFragment extends Fragment {
                         }
 
                     }
+                }
+                else
+                {
+                    Toast.makeText(getContext(),"아직 태어나지 않았어요.",Toast.LENGTH_LONG).show();
                 }
             }
      });
