740c9dc1c6ae06a9f0ca68bb25620b1fd8357054
juho1210 <wngh9512@gmail.com>
2018-12-03 02:09:15 +0900

    게시판 추가 중

--- Baby care/app/src/main/java/com/example/helloworld/myapplication/fragment/BoardFragment.java
+++ Baby care/app/src/main/java/com/example/helloworld/myapplication/fragment/BoardFragment.java
@@ -1,21 +1,36 @@
 package com.example.helloworld.myapplication.fragment;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.helloworld.myapplication.R;
 import com.example.helloworld.myapplication.activity.MainActivity;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 public class BoardFragment extends Fragment {
 
     MainActivity activity;
+    Button addButton;
+
+    int i;
+    MyAdapter mMyAdapter;
+    private ListView mListView;
 
     @Override
     public void onAttach(Context context){
